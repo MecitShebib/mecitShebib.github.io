1630efb8943b24cb47f41f4f10f3d73fe62265b3
MecitŞebib <83697564+Mecit2000@users.noreply.github.com>
2022-04-08 17:53:29 +0300

    Add Script.js file

---
+++ Script.js
@@ -0,0 +1,13 @@
+﻿function contact() {
+    var x = document.getElementById("sağ_para");
+
+    x.style.display = "none";
+    document.getElementById("contact").style.display = "flex";
+}
+function home() {
+    var x = document.getElementById("sağ_para");
+
+    x.style.display = "flex";
+    document.getElementById("contact").style.display = "none";
+
+}
